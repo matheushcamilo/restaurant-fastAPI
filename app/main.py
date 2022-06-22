76254a167a8277419468ffae99a4bd4c73dc969b
matheushcamilo <matheushcamilo1997@gmail.com>
2022-06-21 23:12:00 -0300

    Adding function to get client by name

--- app/main.py
+++ app/main.py
@@ -8,11 +8,17 @@ app = FastAPI()
 
 Base.metadata.create_all(engine)
 
-
+#TODO Fix order of Json response
 @app.get("/clients")
 def get_clients(db: Session = Depends(get_db)):
     return client_repo.get_all(db)
 
+@app.get("/clients/{first_name}-{last_name}")
+def get_client_by_name(first_name: str, last_name: str, db: Session = Depends(get_db)):
+    return client_repo.get_by_name(first_name, last_name, db)
+
+
 @app.post("/clients")
 def create_client(request: schemas.Client, db: Session = Depends(get_db)):
     return client_repo.create(request, db)    
+    
--- app/repository/client_repo.py
+++ app/repository/client_repo.py
@@ -1,4 +1,3 @@
-from distutils.command.build_scripts import first_line_re
 from sqlalchemy.orm import Session
 from .. import models, schemas
 
@@ -7,6 +6,11 @@ def get_all(db: Session):
     clients = db.query(models.Client).all()
     return clients
 
+
+def get_by_name(first_name, last_name, db: Session):
+    return db.query(models.Client).filter(models.Client.first_name == first_name and
+    models.Client.last_name == last_name).first()
+
 def create(request: schemas.Client, db: Session):
     new_client = models.Client(first_name=request.first_name, last_name=request.last_name,
     gender=request.gender)
@@ -14,4 +18,5 @@ def create(request: schemas.Client, db: Session):
     db.commit()
     db.refresh(new_client)
     return new_client
+   
         
